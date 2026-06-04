b14adfa2887b35bde7967e68c99abfc4f6de2699
DMYTRO SHYVACHOV <102044731+Dmitze@users.noreply.github.com>
2026-06-05 00:10:50 +0300

    feat: export utility functions for ML module

--- src/ytaimbot_ml/utils/__init__.py
+++ src/ytaimbot_ml/utils/__init__.py
@@ -2,9 +2,10 @@
 from __future__ import annotations
 
 from ytaimbot_ml.utils.hyperloglog import HyperLogLog, HyperLogLogCounter
+from ytaimbot_ml.utils.random import make_rng # Added import
 
 __all__ = [
     "HyperLogLog",
     "HyperLogLogCounter",
+    "make_rng", # Added to __all__
 ]
-
